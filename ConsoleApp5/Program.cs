e8c130fd808183d072838aa70a29e049f6f238dc
Foksha1488 <maksimfoksha97@gmail.com>
2024-12-03 09:50:35 +0200

    Task 1
    1. Поменять местами содержимое двух строковых переменных.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {///Gоменять местами содержимое двух строковых переменных.
+        static void Swap(ref string x, ref string y)
+    {
+            string p = x;
+            x = y;
+            y = p;
+        }
+        
+         static void Main(string[] args)
+        {
+        string st1 = "Это первая строка";
+        string st2 = "Это вторая строка";
+        Console.WriteLine($"Вывод до обмена:{st1}\t{st2}");
+        Swap(ref st1,ref st2);
+        Console.WriteLine($"Вывод после обмена:{st1}\t{st2}");
+        Console.ReadKey();
+        }
+    }
+}
